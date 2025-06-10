4901118dfc951dc406e673d0cd1d52046611d3d6
LxWrd <fokesart17@gmail.com>
2025-06-10 12:31:57 +0500

    Условие проверки чисел

--- main.py
+++ main.py
@@ -12,35 +12,45 @@ class Tree:
         """Строит бинарное дерево на основе списка пар (число, путь)."""
         head = Tree(0) # Добавляю корень (по условию всегда 0)
         # Цикл проходящий через все кортежи (Значение, Путь) списка
-        for number, path in data:
+        for number, way in data:
+            flag = True
+            try:
+                if isinstance(int(number),int) != True:
+                    print('Ошибка')
+            except ValueError:
+                print(f'Ошибка в значений - {number}')
+                flag = False
+            for i in way:
+                if i != '0' and i != '1':
+                    print(f"Некорректный путь - {way}")
+                    flag = False
             current = head
             # Проходим через путь, при необходимости добавляя узлы, доходя до элемента стоящего перед нужным
-            for bit in path[:-1]:
-                if bit == '0':
-                    if current.left is None:
-                        current.left = Tree(0)
-                    current = current.left
+            if flag:
+                for bit in way[:-1]:
+                    if bit == '0':
+                        if current.left is None:
+                            current.left = Tree(0)
+                        current = current.left
+                    else:
+                        if current.right is None:
+                            current.right = Tree(0)
+                        current = current.right
+                # Берется последний элемент пути и по нему добавляется узел с значением
+                last_bit = way[-1]
+                if last_bit == '0':
+                    current.left = Tree(number)
                 else:
-                    if current.right is None:
-                        current.right = Tree(0)
-                    current = current.right
-            # Берется последний элемент пути и по нему добавляется узел с значением
-            last_bit = path[-1]
-            if last_bit == '0':
-                current.left = Tree(number)
-            else:
-                current.right = Tree(number)
+                    current.right = Tree(number)
 
         return head
 
-    def print_tree(self, level=0, prefix='Head - '):
-        """Выводит дерево в читаемом виде."""
-        if self is not None:
-            print(" " * (level * 4) + prefix + str(self.value))
-            if self.left:
-                self.left.print_tree(level + 1, "L--- ")
-            if self.right:
-                self.right.print_tree(level + 1, "R--- ")
+def print_tree(node, level=0, prefix="Head - "):
+    """Выводит дерево в читаемом виде."""
+    if node is not None:
+        print(" " * (level * 4) + prefix + str(node.value))
+        print_tree(node.left, level + 1, "L--- ")
+        print_tree(node.right, level + 1, "R--- ")
 
 def file_reader():
     """Выводит список с кортежами"""
@@ -57,7 +67,7 @@ def main():
 
     tree = Tree.build_tree(data)
 
-    tree.print_tree()
+    print_tree(tree)
 
 if __name__ == "__main__":
     main()
